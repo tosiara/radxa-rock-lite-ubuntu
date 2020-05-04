568d389b47a8f6b03386e8bb5f36dc02699a061c
tosiara <tosiara@users.noreply.github.com>
2020-05-04 11:45:12 +0300

    Updated related paths

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 echo "Install arm compiler"
 sudo apt-get install git build-essential gcc-arm-linux-gnueabihf lzop libncurses5-dev libssl-dev bc
+sudo -k
 
 if [ -d "./build" ]
 then
@@ -9,19 +10,20 @@ then
 	exit 1
 fi
 
+maindir=`pwd`
+
 echo "mkdir build"
-mkdir build
+mkdir $maindir/build
 
 echo "Compile mkbootimg"
-cd rockchip-mkbootimg
+cd $maindir/rockchip-mkbootimg
 make
-cd ..
 
 echo "Compile kernel"
-cd kernel
+cd $maindir/kernel
 mkdir modules
-cp ../defconfig/rockchip_defconfig arch/arm/configs/rockchip_defconfig
-cp ../dts/rk3188-radxarock-lite.dts arch/arm/boot/dts/rk3188-radxarock.dts
+cp $maindir/defconfig/rockchip_defconfig arch/arm/configs/rockchip_defconfig
+cp $maindir/dts/rk3188-radxarock-lite.dts arch/arm/boot/dts/rk3188-radxarock.dts
 
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabihf-
@@ -32,46 +34,43 @@ make zImage dtbs
 make modules
 make modules_install
 
-cp arch/arm/boot/zImage ../build/
-cp arch/arm/boot/dts/rk3188-radxarock.dtb ../build/
+cp arch/arm/boot/zImage $maindir/build/
+cp arch/arm/boot/dts/rk3188-radxarock.dtb $maindir/build/
 
 echo "Compress initrd"
-cd ../initrd
+cd $maindir/initrd
 #cp ../kernel/modules/lib/modules/4.4.221 lib/modules/ -r
-find . ! -path "./.git*"  | cpio -H newc  -ov > ../build/initrd.img
+find . ! -path "./.git*"  | cpio -H newc  -ov > $maindir/build/initrd.img
 
-cd ../build
+cd $maindir/build
 gzip initrd.img
 cat zImage rk3188-radxarock.dtb > zImage-dtb
-../rockchip-mkbootimg/mkbootimg --kernel zImage-dtb --ramdisk initrd.img.gz -o boot.img
+$maindir/rockchip-mkbootimg/mkbootimg --kernel zImage-dtb --ramdisk initrd.img.gz -o boot.img
 
-cd ..
-
-if [ ! -d "./arm-eabi-4.6" ]
+if [ ! -d "$maindir/arm-eabi-4.6" ]
 then
 	echo "Download u-boot required toolchain"
 	mkdir arm-eabi-4.6
 	cd arm-eabi-4.6
 	wget https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/+archive/refs/heads/kitkat-release.tar.gz
 	tar xvfz kitkat-release.tar.gz
-	cd ..
 fi
 
 echo "Compile u-boot"
-cd u-boot-rockchip
+cd $maindir/u-boot-rockchip
 export ARCH=arm
-export CROSS_COMPILE=`pwd`/../arm-eabi-4.6/bin/arm-eabi-
+export CROSS_COMPILE=$maindir/arm-eabi-4.6/bin/arm-eabi-
 make rk30xx
 ./pack-sd.sh
-cp u-boot-sd.img ../build/
+cp u-boot-sd.img $maindir/build/
 
 echo "Compile rkcrc"
-cd ../rkflashtool
-gcc -o ../build/rkcrc rkcrc.c
+cd $maindir/rkflashtool
+gcc -o $maindir/build/rkcrc rkcrc.c
 
 echo "Make parameter image"
-cd ../parameter
-../build/rkcrc -p parameter_linux_sd ../build/parameter.img  
+cd $maindir/parameter
+$maindir/build/rkcrc -p parameter_linux_sd $maindir/build/parameter.img
 
 echo "Done"
 
